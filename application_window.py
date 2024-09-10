ac3bfa827d85095fd19fa8ab662a67aa6a7fc9e8
nfmmendes <nilson.fmmendes@outlook.com>
2024-09-10 16:21:57 +0200

    Add button to access an open file dialogue

--- application_window.py
+++ application_window.py
@@ -10,6 +10,7 @@ from matplotlib.backends.backend_qtagg import \
     NavigationToolbar2QT as NavigationToolbar
 from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.figure import Figure
+from PyQt6.QtWidgets import QPushButton, QFileDialog
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -32,7 +33,12 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         distance_time_chart_canvas = FigureCanvas(Figure(figsize=(12, 6.5)))
         self._distance_time_chart = distance_time_chart_canvas.figure.subplots()
         self.plotDistanceOverTime(self._distance_time_chart, df)
-        
+       
+        self._open_file_button = QPushButton("Open gpx file")
+        self._open_file_button.setFixedSize(100, 32)
+        self._open_file_button.clicked.connect(self.openFileDialog)
+
+        layout.addWidget(self._open_file_button)
         layout.addWidget(NavigationToolbar(speed_chart_canvas, self))
         layout.addWidget(speed_chart_canvas)
         
@@ -40,6 +46,10 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         layout.addWidget(distance_time_chart_canvas)
  
         self.showMaximized()
+
+    def openFileDialog(self):
+        fname = QFileDialog.getOpenFileName(self,"Open File", "",
+                                            "GPX Files (*.gpx)",)
 
     def plotSpeed(self, chart, df):
         chart.plot(df["KM"], df["Avg Speed"], label="Average speed")
