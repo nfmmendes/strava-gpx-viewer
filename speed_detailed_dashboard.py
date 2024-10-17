4be9a08d4c849c00d2d78d8fc9dd4c852cb6e888
nfmmendes <nilson.fmmendes@outlook.com>
2024-10-17 14:46:31 +0200

    Add speed vs (grade x accum. elevation) chart

--- speed_detailed_dashboard.py
+++ speed_detailed_dashboard.py
@@ -23,9 +23,11 @@ class SpeedDetailedDashboard(QWidget):
         quantile_001, quantile_999 = new_df["Grade"].quantile([0.001, 0.999])
         new_df = new_df[new_df["Grade"].between(quantile_001, quantile_999)]
 
-        self._speed_grade_canvas = FigureCanvas(Figure(figsize=(4,3.2)))
-
+        self._speed_grade_canvas = FigureCanvas(Figure(figsize = (4,3.2)))
+        self._speed_elevation_grade_canvas = FigureCanvas(Figure(figsize = (4, 3.2)))
+        
         layout.addWidget(self._speed_grade_canvas)
+        layout.addWidget(self._speed_elevation_grade_canvas)
 
         xy = np.vstack([new_df["Grade"],new_df["Speed"]])
         z = gaussian_kde(xy)(xy)
@@ -35,4 +37,16 @@ class SpeedDetailedDashboard(QWidget):
         chart.set_xlabel("Grade (%)")
         chart.set_ylabel("Speed (Km/h)")
 
+        new_df["Pos Elevation Gain"] = [ 0 if x < 0 else x for x in new_df["Elevation Gain"]]
+        new_df["Tot. Elevation Gain"] =  new_df["Pos Elevation Gain"].cumsum()
+        new_df["Grade Mult. Elevation"] = new_df["Grade"]*new_df["Tot. Elevation Gain"]/100
+        
+        xy = np.vstack([new_df["Grade Mult. Elevation"], new_df["Speed"]])
+        z = gaussian_kde(xy)(xy)
+ 
+
+        chart = self._speed_elevation_grade_canvas.figure.subplots()
+        chart.scatter(new_df["Grade Mult. Elevation"], new_df["Speed"], c = z, s = 10)
+        chart.set_xlabel("Grade X Total Elevation Gain (m)")
+        chart.set_ylabel("Speed (Km/h)")
 
