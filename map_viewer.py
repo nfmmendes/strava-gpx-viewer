f274be9719d7b08456d7a0622c290e346c3c1d15
nfmmendes <nilson.fmmendes@outlook.com>
2024-12-03 22:08:25 +0100

    Add start and end labels to tracks

--- map_viewer.py
+++ map_viewer.py
@@ -1,6 +1,7 @@
 import folium
 import io
 import PyQt6.QtWebEngineWidgets as qtweb
+from folium.features import DivIcon
 
 class MapViewer:
 
@@ -23,10 +24,17 @@ class MapViewer:
     def show_poly_line(self, points, zoom = 15):
         half = int(len(points)/2)
         self._map = folium.Map(
-                location=[points[half][0], points[half][1]], zoom_start= zoom
+                location=points[half], zoom_start= zoom
         )
+        
         folium.PolyLine(points, color='red', weight=4.5, opacity=.5).add_to(self._map)
 
+        div = lambda text : f'<div style="font-size: 14pt; background-color: white; border-radius:4px; ">{text}</div>'
+        icon_start = DivIcon(icon_size=(40,40), icon_anchor=(0,0), html=div('Start'))
+        icon_end = DivIcon(icon_size=(36,36), icon_anchor=(0,0), html=div('End'))
+        folium.Marker(location=points[0], icon=icon_start).add_to(self._map)
+        folium.Marker(location=points[-1], icon = icon_end).add_to(self._map)
+        
         data = io.BytesIO()
         self._map.save(data, close_file=False)
 
