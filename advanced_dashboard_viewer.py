25608c9ce633783c3179d390a570404445c9fe9e
nfmmendes <nilson.fmmendes@outlook.com>
2026-02-06 16:21:21 -0300

    Document class AdvancedDashboardViewer

--- advanced_dashboard_viewer.py
+++ advanced_dashboard_viewer.py
@@ -1,12 +1,19 @@
 from PyQt6.QtWidgets import QWidget, QTabWidget, QVBoxLayout
 from grade_detailed_dashboard import GradeDetailedDashboard
 from speed_detailed_dashboard import SpeedDetailedDashboard
+from pandas import DataFrame
 
 """
 Window to hold advanced dashboards about the speed and grade data. 
 """
 class AdvancedDashboardViewer(QWidget):
-    def __init__(self, data_frame):
+    def __init__(self, data_frame: DataFrame):
+        """
+        Class constructor
+        
+        :param data_frame: Dataframe containing the data to be used on class object initialization.
+        :type data_frame: pandas.DataFrame
+        """
         super().__init__()
         layout = QVBoxLayout(self)
         self._redraw = False
