a47805e95a4b71ee38f857aa13d95ec1ccda995e
nfmmendes <nilson.fmmendes@outlook.com>
2026-02-06 16:49:57 -0300

    Document class ChartRangeSelector

--- chart_range_selector.py
+++ chart_range_selector.py
@@ -1,20 +1,41 @@
 import matplotlib.pyplot as plt
-import numpy as np
+from typing import Callable
 
-from matplotlib.widgets import EllipseSelector, RectangleSelector
+from matplotlib.widgets import RectangleSelector
+from matplotlib.backend_bases import MouseEvent, MouseButton
 
 class ChartRangeSelector:
-    def __init__(self, ax, select_callback):
+    def __init__(self, ax: plt.Axes, select_callback: Callable[[MouseEvent, MouseButton], None]):
+        """
+        Class constructor.
+
+        :param ax: The axis selected. 
+        :type ax: Axes
+        :param select_callback: The function to be called after the range selection. 
+        :type select_callback: Function[MouseEvent, MouseEvent]
+        """
         self._ax = ax
         self._select_callback = select_callback
 
         self._init_selector()
 
-    def reset(self):
+    def reset(self) -> None:
+        """
+        Reset the range selection
+
+        :return: None
+        :rtype: None
+        """
         self._selector = None
         self._init_selector()
     
     def _init_selector(self):
+        """
+        Init range selection.
+        
+        :return: None
+        :rtype: None
+        """
         self._selector = RectangleSelector(
             self._ax, self._select_callback,
             useblit=True,
